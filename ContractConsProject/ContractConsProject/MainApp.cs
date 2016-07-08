71a29fc3bfffb74b430ebc6e0243ba2cb842a1da
nablam <lamribenn@gmail.com>
2016-07-08 14:11:05 -0400

    base app

---
+++ ContractConsProject/ContractConsProject/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractConsProject
+{
+    public class MainApp
+    {
+
+        public MainApp() { }
+
+        public void DoTimer() {
+
+            while (Console.ReadLine() != "0") {
+                Console.WriteLine("boom");
+            }
+            Console.WriteLine("done");
+
+        }
+    }
+}
